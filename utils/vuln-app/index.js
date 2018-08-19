3def72a87c595441fbd9cd1adc0fc998cdae2b87
AH7 <assaf.he2@gmail.com>
2018-08-19 16:03:32 +0300

    feat: add config and test for methods detection

--- utils/vuln-app/index.js
+++ utils/vuln-app/index.js
@@ -1,5 +1,10 @@
-require('snyk-nodejs-agent')({projectId: 'c4644502-6af6-4ab7-b7d1-78f2e3df103f'});
+require('snyk-nodejs-agent')({projectId: process.PROJECT_PUBLID_ID});
 
 var qs = require("qs");
 var st = require("st");
 var superagent = require('superagent');
+var toughcookie = require('tough-cookie');
+var uglifyjs = require('uglify-js');
+var ms = require('ms');
+var handlebars = require('handlebars');
+var debug = require('debug');
