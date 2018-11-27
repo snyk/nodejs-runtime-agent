ee930027e40a6ccfe73c8f815388ec381090aee1
Chris West (Faux) <git@goeswhere.com>
2018-11-27 12:19:18 +0000

    chore: remove all remaining 'var' in non-fixture tests

--- test/method_detection.test.js
+++ test/method_detection.test.js
@@ -1,37 +1,36 @@
 const test = require('tap').test;
 
-var ast = require('../lib/ast.js');
+const ast = require('../lib/ast.js');
 
 test('test st method detection', function (t) {
-  var stPath = __dirname + '/fixtures/st/node_modules/st.js';
-  var methods = ['Mount.prototype.getPath'];
-  var line = 158;
-  var found = ast.findAllVulnerableFunctionsInScriptPath(
-    stPath, methods
+  const stPath = __dirname + '/fixtures/st/node_modules/st.js';
+  const methods = ['Mount.prototype.getPath'];
+  const line = 158;
+  const found = ast.findAllVulnerableFunctionsInScriptPath(
+    stPath, methods,
   );
   t.equal(found[methods[0]].line, line, 'Mount.prototype.getPath found');
   t.end();
 });
 
 test('test handlebars method detection', function (t) {
-  var stPath = __dirname + '/fixtures/handlebars/lib/handlebars/utils.js';
-  var methods = ['escapeExpression'];
-  var line = 63;
-  var found = ast.findAllVulnerableFunctionsInScriptPath(
-    stPath, methods
+  const stPath = __dirname + '/fixtures/handlebars/lib/handlebars/utils.js';
+  const methods = ['escapeExpression'];
+  const line = 63;
+  const found = ast.findAllVulnerableFunctionsInScriptPath(
+    stPath, methods,
   );
   t.equal(found[methods[0]].line, line, 'escapeExpression found');
   t.end();
 });
 
 test('test uglify-js method detection', function (t) {
-    var stPath = __dirname + '/fixtures/uglify-js/lib/parse.js';
-    var methods = ['parse_js_number'];
-    var line = 180;
-    var found = ast.findAllVulnerableFunctionsInScriptPath(
-      stPath, methods
-    );
-    t.equal(found[methods[0]].line, line, 'parse_js_number found');
-    t.end();
-  });
-  
+  const stPath = __dirname + '/fixtures/uglify-js/lib/parse.js';
+  const methods = ['parse_js_number'];
+  const line = 180;
+  const found = ast.findAllVulnerableFunctionsInScriptPath(
+    stPath, methods,
+  );
+  t.equal(found[methods[0]].line, line, 'parse_js_number found');
+  t.end();
+});
