2a31003200332bee9a370d38c66d9d8c635752bf
Chris West (Faux) <git@goeswhere.com>
2018-11-27 12:25:10 +0000

    chore: lift loading/error handling out of ast

--- test/method_detection.test.js
+++ test/method_detection.test.js
@@ -1,35 +1,36 @@
+const fs = require('fs');
 const test = require('tap').test;
 
 const ast = require('../lib/ast.js');
 
 test('test st method detection', function (t) {
-  const stPath = __dirname + '/fixtures/st/node_modules/st.js';
+  const content = fs.readFileSync(__dirname + '/fixtures/st/node_modules/st.js');
   const methods = ['Mount.prototype.getPath'];
   const line = 158;
-  const found = ast.findAllVulnerableFunctionsInScriptPath(
-    stPath, methods,
+  const found = ast.findAllVulnerableFunctionsInScript(
+    content, methods,
   );
   t.equal(found[methods[0]].line, line, 'Mount.prototype.getPath found');
   t.end();
 });
 
 test('test handlebars method detection', function (t) {
-  const stPath = __dirname + '/fixtures/handlebars/lib/handlebars/utils.js';
+  const content = fs.readFileSync(__dirname + '/fixtures/handlebars/lib/handlebars/utils.js');
   const methods = ['escapeExpression'];
   const line = 63;
-  const found = ast.findAllVulnerableFunctionsInScriptPath(
-    stPath, methods,
+  const found = ast.findAllVulnerableFunctionsInScript(
+    content, methods,
   );
   t.equal(found[methods[0]].line, line, 'escapeExpression found');
   t.end();
 });
 
 test('test uglify-js method detection', function (t) {
-  const stPath = __dirname + '/fixtures/uglify-js/lib/parse.js';
+  const content = fs.readFileSync(__dirname + '/fixtures/uglify-js/lib/parse.js');
   const methods = ['parse_js_number'];
   const line = 180;
-  const found = ast.findAllVulnerableFunctionsInScriptPath(
-    stPath, methods,
+  const found = ast.findAllVulnerableFunctionsInScript(
+    content, methods,
   );
   t.equal(found[methods[0]].line, line, 'parse_js_number found');
   t.end();
